74421527cf897720831fca934cacce11efc3ca41
Anees Ziafat <ziafatanees@gmail.com>
2022-09-27 19:39:53 -0400

    proxy examples

--- hw1/README.md
+++ hw1/README.md
@@ -6,4 +6,18 @@ HTTP server and HTTP proxy server using only the socket library.
 
 The webserver and proxyserver can be run in the terminal with the command "python webserver.py" or "python proxyserver.py" depending on the python installation.
 
-## Webpages tested on
+## Webpages tested on web server (chrome)
+localhost:6789/HelloWorld.html return html file successfully
+
+localhost:6789/sbu.jpg return jpg file successfully
+
+localhost:6789/test/seawolf.png return jpg file successfully
+
+localhost:6789/doesnotexist.html returned 404 successfully
+
+## Webpages tested on proxy (chrome)
+The following files/images were tested and returned image successfully and request response was cached
+http://localhost:8888/http://gaia.cs.umass.edu/wireshark-labs/HTTP-wireshark-file2.html
+http://localhost:8888/http://gaia.cs.umass.edu/wireshark-labs/HTTP-wireshark-file3.html
+http://localhost:8888/https://www.bravofurniture.net/images/catalog_icons/27.jpg  
+
--- hw1/proxyserver.py
+++ hw1/proxyserver.py
@@ -2,34 +2,28 @@ from socket import *
 
 #local host only
 HOST = "127.0.0.1"
-PORT = 6789
+PORT = 8888
 file_cache = {}
 
-def sendRequest(url):
-    #getting rid of https:// or http:// if it exists
-    if "://" in url:
-        url = url.split("://",1)[-1]
-    
+def sendRequest(url): 
     host, seperator, path = url.partition('/')
+    try:
+        serverName = host
+        serverPort = 80
+        clientSocket = socket(AF_INET, SOCK_STREAM)
+        clientSocket.connect((serverName,serverPort))
 
-    #try:
-    serverName = host
-    serverPort = 80
-    clientSocket = socket(AF_INET, SOCK_STREAM)
-    clientSocket.connect((serverName,serverPort))
+        #request to send to server
+        request = f"GET /{path} HTTP/1.1\r\nHost:{host}\r\n\r\n"
+        clientSocket.send(request.encode())
 
-    #request to send to server
-    request = f"GET /{path} HTTP/1.1\r\nHost:{host}\r\n\r\n"
-    clientSocket.send(request.encode())
-
-    #response from server
-    response = clientSocket.recv(8192)
-    clientSocket.close()
-
-    return response
-    #except:
+        #response from server
+        response = clientSocket.recv(64000)
+        clientSocket.close()
+        return response
+    except:
         #get request was not successful
-        #return 'HTTP/1.1 404 Not Found\n\n'
+        return 'HTTP/1.1 404 Not Found\n\n'.encode()
 
 serverSocket = socket(AF_INET,SOCK_STREAM)
 serverSocket.bind((HOST,PORT))
@@ -38,25 +32,34 @@ print("Server is listening on port: " + str(PORT))
 while True:
     connectionSocket, addr = serverSocket.accept()
     request = connectionSocket.recv(1024).decode()
-    
+
     #parsing http request
     fields = request.split('\r\n', 1)
     requestInfo = fields[0].split(" ")
 
     #if its not a get request or file is not found send 404 not found 
-    response = 'HTTP/1.1 404 Not Found\n\n'
+    response = 'HTTP/1.1 404 Not Found\n\n'.encode()
+    
+    #sometimes empty request is sent causing error
+    if len(requestInfo) < 2:
+        print("empty error")
+        continue
+
     request_url = requestInfo[1][1:]
+     #getting rid of https:// or http:// if it exists
+    if "://" in request_url:
+        request_url = request_url.split("://",1)[-1]
     
     check_cache = file_cache.get(request_url)
     if check_cache != None:
         #if response is cached send that
-        #print("Was cached")
+        print("Sent cached request")
         response = check_cache
     else:
         #if response if not cached, sent request to url and cache the response
         response = sendRequest(request_url)
         file_cache[request_url] = response
-        #print("Was not cached, caching")
+        print("Was not cached, made request")
 
 
     connectionSocket.send(response)
